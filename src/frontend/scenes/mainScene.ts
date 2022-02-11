9aea34301c03f6e29ce26c48fdf2fd6d11a72dd0
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2022-02-11 16:56:29 +0700

    remove redundant code and shift a tab

--- src/frontend/scenes/mainScene.ts
+++ src/frontend/scenes/mainScene.ts
@@ -336,56 +336,56 @@ export class MainScene extends Phaser.Scene {
     //this.add.text(500,300,"press nonwhere to hop", {fontSize:"50px"}).setOrigin(.5,.5)
 
     //this.playerLabel =  this.add.text(-50,-50," this is you").setOrigin(.5,1)
-    this.playersConnectedText = this.add.text(20,20,"")
-    this.matter.world.setBounds(0,0,1024,750, 50,true, true, true, true)
+    //this.playersConnectedText = this.add.text(20,20,"")
+    //this.matter.world.setBounds(0,0,1024,750, 50,true, true, true, true)
     
-      this.socket = io()
-      this.socket.on("first hi", (data: UserData, opponentData: UserData[])=>{
-        if(this.firstHi != true){
-          this.firstHi = true
-        this.player = new Square(this, data)     
-        opponentData.forEach((o)=>{
-          let opponent = new Square(this, o)     
-          this.opponents.push(opponent)
-        })
-        this.time.addEvent({ delay: 1000/60,  loop: true, callback: this.updateState(), callbackScope: this });
-        }        
-      })
-
-      this.socket.on("add opponent", (data: UserData)=>{
-        let opponent = new Square(this, data)     
+    this.socket = io()
+    this.socket.on("first hi", (data: UserData, opponentData: UserData[])=>{
+      if(this.firstHi != true){
+        this.firstHi = true
+      this.player = new Square(this, data)     
+      opponentData.forEach((o)=>{
+        let opponent = new Square(this, o)     
         this.opponents.push(opponent)
-         
       })
-      this.socket.on("remove player", (pSocket)=>{
-        /*let o:Square[] = this.opponents.filter((player:Square) => { return player.socketId == pSocket})
-        if(o && o[0]){
-          let p = o[0]
-          this.opponents.splice(this.opponents.indexOf(p, 1))
-          p.destroy()
+      this.time.addEvent({ delay: 1000/60,  loop: true, callback: this.updateState(), callbackScope: this });
+      }        
+    })
 
+    this.socket.on("add opponent", (data: UserData)=>{
+      let opponent = new Square(this, data)     
+      this.opponents.push(opponent)
+        
+    })
+    this.socket.on("remove player", (pSocket)=>{
+      /*let o:Square[] = this.opponents.filter((player:Square) => { return player.socketId == pSocket})
+      if(o && o[0]){
+        let p = o[0]
+        this.opponents.splice(this.opponents.indexOf(p, 1))
+        p.destroy()
+
+      }*/
+    })
+    this.socket.on("update all", (data: any[])=>{
+      data.forEach((p)=>{
+        /*let o:Square[] = this.opponents.filter((player:Square) => { return player.socketId == p.socketId})       
+        if(o && o[0] && o[0].socketId != this.player.socketId){
+          let opponent = o[0]
+          opponent.x = p.x
+          opponent.y = p.y
+          opponent.setVelocityX(p.vx)
+          opponent.setVelocityY(p.vY)
+          opponent.angle = p.angle
         }*/
-      })
-      this.socket.on("update all", (data: any[])=>{
-        data.forEach((p)=>{
-          /*let o:Square[] = this.opponents.filter((player:Square) => { return player.socketId == p.socketId})       
-          if(o && o[0] && o[0].socketId != this.player.socketId){
-            let opponent = o[0]
-            opponent.x = p.x
-            opponent.y = p.y
-            opponent.setVelocityX(p.vx)
-            opponent.setVelocityY(p.vY)
-            opponent.angle = p.angle
-          }*/
 
-        })       
-      })  
-      
-      this.socket.emit("ready")    
-      this.input.on("pointerdown", ()=>{
-        if(this.player.y>700)
-        this.player.applyForce(new Phaser.Math.Vector2(.025-.05*Math.random(), -.05-.125*Math.random()))
-      })     
+      })       
+    })  
+    
+    this.socket.emit("ready")    
+    this.input.on("pointerdown", ()=>{
+      if(this.player.y>700)
+      this.player.applyForce(new Phaser.Math.Vector2(.025-.05*Math.random(), -.05-.125*Math.random()))
+    })     
   }
 
   update(){
